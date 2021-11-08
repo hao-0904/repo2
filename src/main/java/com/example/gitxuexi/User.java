7bb4744a6618bf15f7441b350b4361a67038d2b8
songhao <2633571207@qq.com>
2021-11-08 12:08:24 +0800

    第一次添加

---
+++ src/main/java/com/example/gitxuexi/User.java
@@ -0,0 +1,8 @@
+package com.example.gitxuexi;
+
+public class User {
+    public static void main(String[] args) {
+        System.out.println("Hello,Word");
+        System.out.println("hahaha");
+    }
+}
